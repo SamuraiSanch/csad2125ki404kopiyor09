47dcfaa3cc3556ccd3fccda6aa7b1d67ca6d104b
Alex <sashakopiy@icloud.com>
2024-10-21 18:31:21 +0300

    Comments fix

--- ci/build.sh
+++ ci/build.sh
@@ -1,14 +1,10 @@
 с#!/bin/bash
 
-# Створюємо директорію для збірки
 mkdir -p build
 cd build
 
-# Використовуємо CMake для генерації збірочних файлів
 cmake ..
 
-# Збираємо проект
 cmake --build . --config Release
 
-# Повертаємось назад
 cd ..
